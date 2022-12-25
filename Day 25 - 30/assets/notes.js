bc74d7975fa365fd16382f5fbc01bb000ad5383e
Utkarsh Goel <66838142+utkarshgoel10@users.noreply.github.com>
2022-12-25 18:30:15 +0530

    Create notes.js

---
+++ Day 25 - 30/assets/notes.js
@@ -0,0 +1,28 @@
+const notes = [
+  {
+    _id: "1",
+    title: "Day 1 of Bootcamp",
+    content: "I learnt basics of Web, and learnt HTML.",
+    category: "Intro",
+  },
+  {
+    _id: "2",
+    title: "Learned some CSS",
+    content: "Learned how to create good looking pages.",
+    category: "Learning",
+  },
+  {
+    _id: "3",
+    title: "Started JavaScript",
+    content: "Jumped into JavaScript & bootstrap designs",
+    category: "Entertainment",
+  },
+  {
+    _id: 4,
+    title: "Started React JS",
+    content: "Made my first App in React JS, feels awesome.",
+    category: "Learning",
+  },
+];
+
+module.exports = notes;
